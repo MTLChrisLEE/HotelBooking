4de412150587010365a52ab0b9adc9c854ce19f9
MTLChrisLEE <sanghyukchrislee@gmail.com>
2017-08-24 00:47:32 -0400

    Deletes unnecessary codes (had been commented)

--- src/sample/DataModel/DataSource.java
+++ src/sample/DataModel/DataSource.java
@@ -1,7 +1,5 @@
 package sample.DataModel;
 
-import javafx.fxml.FXML;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,7 +11,7 @@ public class DataSource {
 
     public static final String DB_NAME = "hotelmtlchrislee";
     public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/" + DB_NAME;
-    public static final String TABLE_GUESTS = "guests";
+    public static final String TABLE_GUESTS = "Guests";
     public static final String TABLE_ROOMS = "rooms";
     public static final String TABLE_RESERVATION = "reservation";
 
@@ -23,115 +21,79 @@ public class DataSource {
     public static final String COLUMN_PHONE = "phoneNumber";
     public static final String COLUMN_EMAIL = "email";
     public static final String COLUMN_TYPE = "Type";
-    public static final String COLUMN_RATE ="rate";
+    public static final String COLUMN_RATE = "rate";
 
     public static final String COLUMN_ROOM = "RoomNumber";
     public static final String COLUMN_CHECKIN = "checkinDate";
     public static final String COLUMN_CHECKOUT = "checkoutDate";
 
     public static final String QUERY_RESERVATION_BY_GUEST_ID_START =
-            "SELECT " + TABLE_RESERVATION + "." +COLUMN_ROOM+", "+ TABLE_RESERVATION + "." +COLUMN_GUESTID+", "+
-                    TABLE_RESERVATION+"."+COLUMN_CHECKIN+", "+TABLE_RESERVATION+"."+COLUMN_CHECKOUT+ " FROM "+TABLE_RESERVATION+
-                    " INNER JOIN "+ TABLE_GUESTS +" ON " + TABLE_RESERVATION + "."+COLUMN_GUESTID+"="+TABLE_GUESTS+"."+COLUMN_GUESTID+
-                    " WHERE "+ TABLE_GUESTS+"."+COLUMN_GUESTID+"=\'";
+            "SELECT " + TABLE_RESERVATION + "." + COLUMN_ROOM + ", " + TABLE_RESERVATION + "." + COLUMN_GUESTID + ", " +
+                    TABLE_RESERVATION + "." + COLUMN_CHECKIN + ", " + TABLE_RESERVATION + "." + COLUMN_CHECKOUT + " FROM " + TABLE_RESERVATION +
+                    " INNER JOIN " + TABLE_GUESTS + " ON " + TABLE_RESERVATION + "." + COLUMN_GUESTID + "=" + TABLE_GUESTS + "." + COLUMN_GUESTID +
+                    " WHERE " + TABLE_GUESTS + "." + COLUMN_GUESTID + "=\'";
 
     public static final String QEURY_ROOMS = "SELECT * FROM " + TABLE_ROOMS;
 
     public static final String QUERY_RESERVATION = "SELECT * FROM " + TABLE_RESERVATION;
 
 
+    public static final String QUERY_VACANCY = "SELECT r.* FROM " + TABLE_ROOMS + " r WHERE NOT EXISTS (SELECT 1 FROM " + TABLE_RESERVATION + " re WHERE " +
+            "re." + COLUMN_ROOM + "=" + "r." + COLUMN_ROOM + " AND " + "((?>=re." + COLUMN_CHECKIN + " AND ?<re." + COLUMN_CHECKOUT
+            + ") OR " + "(?<re." + COLUMN_CHECKOUT + " AND ?>=re." + COLUMN_CHECKIN + ")))";
 
-    public static final String QUERY_VACANCY = "SELECT r.* FROM " + TABLE_ROOMS + " r WHERE NOT EXISTS (SELECT 1 FROM " + TABLE_RESERVATION + " re WHERE "+
-                                            "re."+COLUMN_ROOM+"="+"r."+COLUMN_ROOM+ " AND " + "((\'2017-08-31\'>=re."+COLUMN_CHECKIN+" AND '2017-08-31'<re."+COLUMN_CHECKOUT
-                                            + ") OR " + "('2017-08-31'<re."+ COLUMN_CHECKOUT + " AND '2017-08-31'>=re." + COLUMN_CHECKIN+")))";
-
-
-
-
-    public static final String QUERY_GUEST_BY_LASTNAME =
-            "SELECT " + TABLE_GUESTS + " WHERE " + COLUMN_LASTNAME+"=\'";
+    public static final String QUERY_INSERT_GUEST = "INSERT INTO "+  TABLE_GUESTS + " VALUES " + "(?,?,?,?,?)";
+    public static final String QUERY_INSERT_RESERVATION = "INSERT INTO "+ TABLE_RESERVATION + " VALUES " + "(?,?,?,?)";
 
 
-    public static final int ORDER_BY_NONE = 1;
-    public static final int ORDER_BY_ASC =2;
-    public static final int ORDER_BY_DESC=3;
+    private PreparedStatement queryVacancy;
 
     private Connection connection;
 
     private static DataSource instance = new DataSource();
 
-    public static DataSource getInstance(){
+    public static DataSource getInstance() {
         return instance;
     }
 
-    public boolean open(){
-        try{
-            connection = DriverManager.getConnection(CONNECTION_STRING,"root","");
-            return  true;
-        }catch(SQLException e){
-            System.out.println("CANNOT Connect to the DB: "+ e.getMessage());
+    public boolean open() {
+        try {
+            connection = DriverManager.getConnection(CONNECTION_STRING, "root", "");
+            queryVacancy = connection.prepareStatement(QUERY_VACANCY);
+
+            return true;
+        } catch (SQLException e) {
+            System.out.println("CANNOT Connect to the DB: " + e.getMessage());
             return false;
         }
     }
 
-    public void close(){
-        try{
-            if(connection !=null){
+    public void close() {
+        try {
+            if(queryVacancy!=null){
+                queryVacancy.close();
+            }
+            if (connection != null) {
                 connection.close();
             }
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println("CANNOT Close the connection: " + e.getMessage());
         }
     }
 
-    public List<guests> queryGuests(){
-        try(Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM " + TABLE_GUESTS)){
 
-
-            List<guests> guests= new ArrayList<>();
-            while(resultSet.next()){
-                guests guest = new guests();
-                guest.setGuestID(resultSet.getString(COLUMN_GUESTID));
-                guest.setLastName(resultSet.getString(COLUMN_LASTNAME));
-                guest.setFirstName(resultSet.getString(COLUMN_FIRSTNAME));
-                guest.setPhoneNumber((resultSet.getString(COLUMN_PHONE)));
-                guest.setEmail(resultSet.getString(COLUMN_EMAIL));
-                guests.add(guest);
-            }
-
-            return guests;
-
-        }catch(SQLException e){
-            System.out.println("QUERY FAILED: " + e.getMessage());
-            return null;
-
-        }
-
-    }
-
-    public List<reservation> queryReservation(int sortOrder){
+    public List<Reservation> queryReservation(int sortOrder) {
 
         StringBuilder sb = new StringBuilder("SELECT * FROM ");
         sb.append(TABLE_RESERVATION);
-        if(sortOrder!=ORDER_BY_NONE){
-            sb.append(" ORDER BY ");
-            sb.append(COLUMN_ROOM);
-            if(sortOrder==ORDER_BY_DESC){
-                sb.append(" DESC");
-            }else{
-                sb.append(" ASC");
-            }
-        }
+
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sb.toString())) {
 
 
-        try(Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sb.toString())){
-
-
-            List<reservation> reservations= new ArrayList<>();
-            while(resultSet.next()){
-                reservation reservation = new reservation();
+            List<Reservation> reservations = new ArrayList<>();
+            while (resultSet.next()) {
+                Reservation reservation = new Reservation();
                 reservation.setGuestID(resultSet.getString(COLUMN_GUESTID));
                 reservation.setRoomNumber(resultSet.getInt(COLUMN_ROOM));
                 reservation.setCheckinDate(resultSet.getDate(COLUMN_CHECKIN));
@@ -141,7 +103,7 @@ public class DataSource {
 
             return reservations;
 
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println("QUERY FAILED: " + e.getMessage());
             return null;
 
@@ -150,41 +112,33 @@ public class DataSource {
     }
 
 
-    public List<rooms> queryAllRooms(){
+    public List<Rooms> queryAllRooms() {
         StringBuilder sb = new StringBuilder(QEURY_ROOMS);
-        try(Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sb.toString())){
-
-
-            List<rooms> rooms= new ArrayList<>();
-            while(resultSet.next()){
-                rooms room = new rooms();
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sb.toString())) {
+            List<Rooms> rooms = new ArrayList<>();
+            while (resultSet.next()) {
+                Rooms room = new Rooms();
                 room.setRoomNumber(resultSet.getInt(COLUMN_ROOM));
                 room.setType(resultSet.getString(COLUMN_TYPE));
                 room.setRate(resultSet.getInt(COLUMN_RATE));
-
                 rooms.add(room);
             }
-
             return rooms;
-
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println("QUERY FAILED: " + e.getMessage());
             return null;
-
         }
     }
 
 
-    public List<reservation> queryAllReservation(){
+    public List<Reservation> queryAllReservation() {
         StringBuilder sb = new StringBuilder(QUERY_RESERVATION);
-        try(Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sb.toString())){
-
-
-            List<reservation> reservations = new ArrayList<>();
-            while(resultSet.next()){
-                reservation reservation = new reservation();
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(sb.toString())) {
+            List<Reservation> reservations = new ArrayList<>();
+            while (resultSet.next()) {
+                Reservation reservation = new Reservation();
                 reservation.setGuestID(resultSet.getString(COLUMN_GUESTID));
                 reservation.setRoomNumber(resultSet.getInt(COLUMN_ROOM));
                 reservation.setCheckinDate(resultSet.getDate(COLUMN_CHECKIN));
@@ -192,86 +146,62 @@ public class DataSource {
                 reservations.add(reservation);
             }
             return reservations;
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println("QUERY FAILED: " + e.getMessage());
             return null;
         }
     }
 
-
-    //SELECT reservation.RoomNumber, reservation.checkinDate, reservation.checkoutDate FROM reservation INNER JOIN guests ON reservation.GuestID=guests.GuestID WHERE guests.GuestID='MTLChrisLEE'
-    public List<reservation> queryReservationbyGuestID(String guestID, int sortOrder){
+    public List<Reservation> queryReservationbyGuestID(String guestID, int sortOrder) {
         StringBuilder sb = new StringBuilder(QUERY_RESERVATION_BY_GUEST_ID_START);
         sb.append(guestID);
         sb.append("\'");
 
-        if(sortOrder!=ORDER_BY_NONE){
-            sb.append(" ORDER BY ");
-            sb.append(COLUMN_CHECKIN);
-            if(sortOrder==ORDER_BY_DESC){
-                sb.append(" DESC");
-            }else{
-                sb.append(" ASC");
-            }
-        }
-
-        //System.out.println(sb.toString());
-
-        try(Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sb.toString())){
-
-
-            List<reservation> reservations= new ArrayList<>();
-            while(resultSet.next()){
-                reservation reservation = new reservation();
+        try (Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(sb.toString())) {
+            List<Reservation> reservations = new ArrayList<>();
+            while (resultSet.next()) {
+                Reservation reservation = new Reservation();
                 reservation.setGuestID(resultSet.getString(COLUMN_GUESTID));
                 reservation.setRoomNumber(resultSet.getInt(COLUMN_ROOM));
                 reservation.setCheckinDate(resultSet.getDate(COLUMN_CHECKIN));
                 reservation.setCheckoutDate((resultSet.getDate(COLUMN_CHECKOUT)));
                 reservations.add(reservation);
             }
-
             return reservations;
-
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println("QUERY FAILED: " + e.getMessage());
             return null;
-
         }
     }
 
+    public List<Rooms> showSearchResult(String date) {
+        try {
+            queryVacancy.setString(1, date);
+            queryVacancy.setString(2, date);
+            queryVacancy.setString(3, date);
+            queryVacancy.setString(4, date);
+            ResultSet resultSet = queryVacancy.executeQuery();
 
-
-    public List<rooms> showSearchResult(){
-        StringBuilder sb = new StringBuilder(QUERY_VACANCY);
-
-        try(Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sb.toString())){
-
-
-            List<rooms> rooms= new ArrayList<>();
-            while(resultSet.next()){
-                rooms room = new rooms();
+            List<Rooms> rooms = new ArrayList<>();
+            while (resultSet.next()) {
+                Rooms room = new Rooms();
                 room.setRoomNumber(resultSet.getInt(COLUMN_ROOM));
                 room.setType(resultSet.getString(COLUMN_TYPE));
                 room.setRate(resultSet.getInt(COLUMN_RATE));
 
                 rooms.add(room);
             }
-
             return rooms;
-
-        }catch(SQLException e){
+        } catch (SQLException e) {
             System.out.println("QUERY FAILED: " + e.getMessage());
             return null;
-
         }
-
-
     }
-
-
-
-
-
 }
+
+
+
+
+
+
